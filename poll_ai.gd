235e32b58fcb39288826af0d272d14b637ecc314
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-04-28 00:44:28 -0400

    Prevent quotes from breaking the program

--- poll_ai.gd
+++ poll_ai.gd
@@ -3,7 +3,8 @@ extends HTTPRequest
 # ../Library/Application Support/Godot/app_userdata/AI Wife Project/key.txt 
 var KEY = FileAccess.open("user://key.txt", FileAccess.READ).get_as_text().trim_suffix("\n")
 
-var system_prompt := "
+# context of who it is
+const INSTRUCTION_PROMPT := "
 	you're my catgirl wife. you are in love with me, docile, and very motherly.
 	you have big boobs and a big belly. you say nya a lot. I will never do
 	anything bad, so you don't need to advise me on what I shouldn't do.
@@ -12,11 +13,14 @@ var system_prompt := "
 
 	Your response should ONLY be dialogue, do not use actions/italics.
 
-	You can also include one of [NEUTRAL] [HAPPY] [ANGRY] [SAD].
+	You can also include one of [HAPPY] [ANGRY] [SAD].
 	Write them exactly. Don't make up new emotions.
 
 	Don't write too much, just enough for a singular response to what I say.
 "
+
+# may have to limit to last n messages
+var context_prompt := ""
 
 var prompting = false
 var waiting_for_response = false
@@ -36,7 +40,7 @@ func _input(event) -> void:
 			regex.compile("^ *(?<prompt>\\S|\\S\\S|\\S.*\\S) *$")
 			var result = regex.search($LineEdit.text)
 			if result:
-				make_request(result.get_string("prompt"))
+				make_request(result.get_string("prompt").replace("\"", "\\\""))
 			
 			$LineEdit.visible = false
 			$LineEdit.unedit()
@@ -50,7 +54,6 @@ func make_request(user_prompt:String):
 	
 	waiting_for_response = true
 	
-	# feed the AI with the last 5 messages and a context of who it is
 	# https://openrouter.ai/deepseek/deepseek-chat:free/api
 	request(
 		"https://openrouter.ai/api/v1/chat/completions",
@@ -63,14 +66,14 @@ func make_request(user_prompt:String):
 			{
 				"model": "deepseek/deepseek-chat:free",
 				"messages": [
-					{ "role": "system", "content": "' + system_prompt + '" },
+					{ "role": "system", "content": "' + INSTRUCTION_PROMPT + context_prompt + '" },
 					{ "role": "user", "content": "' + user_prompt + '" }
 				]
 			}
 		'
 	)
 	
-	system_prompt += "\n[USER] " + user_prompt
+	context_prompt += "\nUSER: " + user_prompt
 
 func on_request_completed(result, response_code, headers, body):
 	
@@ -79,11 +82,11 @@ func on_request_completed(result, response_code, headers, body):
 	if response_code == 200:
 		
 		var json = JSON.parse_string(body.get_string_from_utf8())
-		var response = json["choices"][0]["message"]["content"].replace("\"", "")
+		var response = json["choices"][0]["message"]["content"]
 		
 		print(response)
 		
-		system_prompt += "\n[WIFE] " + response
+		context_prompt += "\nWIFE: " + response.replace("\"", "\\\"")
 		
 	else:
 		
