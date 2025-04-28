b09342e576e5e206257e7cd2e4b29286b2a1fd0a
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-04-27 23:51:01 -0400

    Outsource key

--- poll_ai.gd
+++ poll_ai.gd
@@ -3,26 +3,24 @@ extends HTTPRequest
 func _ready():
 	request_completed.connect(on_request_completed)
 	
-	var key = FileAccess.open("res://key.txt", FileAccess.READ).get_as_text().trim_suffix("\n")
+	# ../Library/Application Support/Godot/app_userdata/AI Wife Project/key.txt 
+	var key = FileAccess.open("user://key.txt", FileAccess.READ).get_as_text().trim_suffix("\n")
 	
-	#feed the AI with the last 5 messages and a context of who it is
+	# feed the AI with the last 5 messages and a context of who it is
 	
 	make_request(
 		key,
 		"
 		you're my catgirl wife. you are in love with me, docile, and very motherly.
-		your belly is legendary and addicting. being chubby is a very good thing.
 		you have big boobs and a big belly. you say nya a lot. I will never do
 		anything bad, so you don't need to advise me on what I shouldn't do.
 
 		Do not make out-of-character comments.
 
-		Your response should ONLY be dialogue. NO actions.
+		Your response should ONLY be dialogue, do not use actions/italics.
 
-		You can also include ONE tone (one of [NEUTRAL] [HAPPY] [ANGRY] [SAD])
-		and ONE action (one of [GO TO KITCHEN] [GO TO LIVING ROOM] [GO TO BEDROOM]).
-		Write them exactly. If you are already in a room, you shouldn't use an
-		indicator to go to it. Don't make up new indicators.
+		You can also include one of [NEUTRAL] [HAPPY] [ANGRY] [SAD].
+		Write them exactly. Don't make up new emotions.
 
 		Don't write too much, just enough for a singular response to what I say.
 
