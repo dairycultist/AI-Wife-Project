34e4dd786925e738abfe47b6302a520190464cd9
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-04-28 00:24:39 -0400

    Good text input

--- poll_ai.gd
+++ poll_ai.gd
@@ -3,39 +3,51 @@ extends HTTPRequest
 # ../Library/Application Support/Godot/app_userdata/AI Wife Project/key.txt 
 var KEY = FileAccess.open("user://key.txt", FileAccess.READ).get_as_text().trim_suffix("\n")
 
+var prompting = false
+
 func _input(event) -> void:
 		
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("enter"):
 		
-		print("enter")
-		# if enter is pressed, show line edit and select line edit.
-		# if enter is pressed again and line edit has text, make a request
+		prompting = not prompting
+		
+		if prompting:
+			$LineEdit.visible = true
+			$LineEdit.edit()
+		else:
+			
+			var regex = RegEx.new()
+			regex.compile("^ *(?<prompt>\\S|\\S\\S|\\S.*\\S) *$")
+			var result = regex.search($LineEdit.text)
+			if result:
+				print(result.get_string("prompt"))
+			
+			$LineEdit.visible = false
+			$LineEdit.unedit()
+			$LineEdit.text = ""
+		
+		# feed the AI with the last 5 messages and a context of who it is
+		#make_request(
+			#"
+			#you're my catgirl wife. you are in love with me, docile, and very motherly.
+			#you have big boobs and a big belly. you say nya a lot. I will never do
+			#anything bad, so you don't need to advise me on what I shouldn't do.
+#
+			#Do not make out-of-character comments.
+#
+			#Your response should ONLY be dialogue, do not use actions/italics.
+#
+			#You can also include one of [NEUTRAL] [HAPPY] [ANGRY] [SAD].
+			#Write them exactly. Don't make up new emotions.
+#
+			#Don't write too much, just enough for a singular response to what I say.
+			#",
+			#"Hey my beautiful catgirl wife~ did you miss me??"
+		#)
 
 
 func _ready():
 	request_completed.connect(on_request_completed)
-	
-	# feed the AI with the last 5 messages and a context of who it is
-	
-	make_request(
-		"
-		you're my catgirl wife. you are in love with me, docile, and very motherly.
-		you have big boobs and a big belly. you say nya a lot. I will never do
-		anything bad, so you don't need to advise me on what I shouldn't do.
-
-		Do not make out-of-character comments.
-
-		Your response should ONLY be dialogue, do not use actions/italics.
-
-		You can also include one of [NEUTRAL] [HAPPY] [ANGRY] [SAD].
-		Write them exactly. Don't make up new emotions.
-
-		Don't write too much, just enough for a singular response to what I say.
-
-		You are in the kitchen.
-		",
-		"Hey my beautiful catgirl wife~ did you miss me??"
-	)
 
 func make_request(system_prompt:String, user_prompt:String):
 	
