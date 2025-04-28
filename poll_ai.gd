5019c51616bd753106b4e50db8d9527b75618d2a
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-04-28 00:33:56 -0400

    Most simple conversations

--- poll_ai.gd
+++ poll_ai.gd
@@ -3,11 +3,27 @@ extends HTTPRequest
 # ../Library/Application Support/Godot/app_userdata/AI Wife Project/key.txt 
 var KEY = FileAccess.open("user://key.txt", FileAccess.READ).get_as_text().trim_suffix("\n")
 
+var system_prompt := "
+	you're my catgirl wife. you are in love with me, docile, and very motherly.
+	you have big boobs and a big belly. you say nya a lot. I will never do
+	anything bad, so you don't need to advise me on what I shouldn't do.
+
+	Do not make out-of-character comments.
+
+	Your response should ONLY be dialogue, do not use actions/italics.
+
+	You can also include one of [NEUTRAL] [HAPPY] [ANGRY] [SAD].
+	Write them exactly. Don't make up new emotions.
+
+	Don't write too much, just enough for a singular response to what I say.
+"
+
 var prompting = false
+var waiting_for_response = false
 
 func _input(event) -> void:
 		
-	if event.is_action_pressed("enter"):
+	if event.is_action_pressed("enter") and not waiting_for_response:
 		
 		prompting = not prompting
 		
@@ -20,37 +36,21 @@ func _input(event) -> void:
 			regex.compile("^ *(?<prompt>\\S|\\S\\S|\\S.*\\S) *$")
 			var result = regex.search($LineEdit.text)
 			if result:
-				print(result.get_string("prompt"))
+				make_request(result.get_string("prompt"))
 			
 			$LineEdit.visible = false
 			$LineEdit.unedit()
 			$LineEdit.text = ""
-		
-		# feed the AI with the last 5 messages and a context of who it is
-		#make_request(
-			#"
-			#you're my catgirl wife. you are in love with me, docile, and very motherly.
-			#you have big boobs and a big belly. you say nya a lot. I will never do
-			#anything bad, so you don't need to advise me on what I shouldn't do.
-#
-			#Do not make out-of-character comments.
-#
-			#Your response should ONLY be dialogue, do not use actions/italics.
-#
-			#You can also include one of [NEUTRAL] [HAPPY] [ANGRY] [SAD].
-			#Write them exactly. Don't make up new emotions.
-#
-			#Don't write too much, just enough for a singular response to what I say.
-			#",
-			#"Hey my beautiful catgirl wife~ did you miss me??"
-		#)
 
 
 func _ready():
 	request_completed.connect(on_request_completed)
 
-func make_request(system_prompt:String, user_prompt:String):
+func make_request(user_prompt:String):
 	
+	waiting_for_response = true
+	
+	# feed the AI with the last 5 messages and a context of who it is
 	# https://openrouter.ai/deepseek/deepseek-chat:free/api
 	request(
 		"https://openrouter.ai/api/v1/chat/completions",
@@ -69,16 +69,25 @@ func make_request(system_prompt:String, user_prompt:String):
 			}
 		'
 	)
+	
+	system_prompt += "\n[USER] " + user_prompt
 
 func on_request_completed(result, response_code, headers, body):
+	
+	waiting_for_response = false
 	
 	if response_code == 200:
 		
 		var json = JSON.parse_string(body.get_string_from_utf8())
-		print(json["choices"][0]["message"]["content"])
+		var response = json["choices"][0]["message"]["content"].replace("\"", "")
+		
+		print(response)
+		
+		system_prompt += "\n[WIFE] " + response
 		
 	else:
 		
+		# error!
 		print(str(result) + "\n\n")
 		print(str(response_code) + "\n\n")
 		print(str(headers) + "\n\n")
